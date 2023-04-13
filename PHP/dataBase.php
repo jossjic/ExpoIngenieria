45c82274ac17f23b32f1c7b7dc5507c3e57f0aa2
jossjic <jossjic_03@hotmail.com>
2023-04-13 16:29:23 -0600

    dataBasePHP con conexión

--- PHP/dataBase.php
+++ PHP/dataBase.php
@@ -1,1 +1,18 @@
-inicio del php
+<?php
+
+
+$dbhost = "Localhost";
+$dbuser = "TC2005B_401_5";
+$dbpass = "b#-ap+E4ru97?STi";
+$dbname = "TC2005B_401_5";
+
+
+$conn= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+if (!$conn)
+{
+   die("NO hay conexión: ".mysqli_connect_error());
+}
+
+
+
+?>
