1ff50299dfb857bff6e94053fece7d4ce25ac6bf
jossjic <jossjic_03@hotmail.com>
2023-04-17 15:56:39 -0600

    mysqli->PDO

--- PHP/dataBase.php
+++ PHP/dataBase.php
@@ -1,16 +1,19 @@
 <?php
 	class Database {
 		private static $dbName 					= 'TC2005B_401_5';
-		private static $dbHost 					= 'Localhost';
+		private static $dbHost 					= 'localhost';
 		private static $dbUsername 			= 'TC2005B_401_5';
 		private static $dbUserPassword 	= 'b#-ap+E4ru97?STi';
 
 		private static $cont  = null;
+		
 
 		public function __construct() {
 			exit('Init function is not allowed');
 		}
+		
 
+		
 		public static function connect(){
 		   // One connection through whole application
 	    	if ( null == self::$cont ) {
@@ -28,4 +31,6 @@
 			self::$cont = null;
 		}
 	}
+	
+	
 ?>
