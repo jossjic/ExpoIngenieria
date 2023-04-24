2e4ab088abc5aec00fa20e8956b4719f0e6f5ac9
Hedguhar Domínguez <hedguhar@vauxoo.com>
2023-04-23 23:04:28 -0500

    [ADD] *: add php index file

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/ico" href="./media/favicon.ico"/>
+
+    <title>Expo Ingenieria</title>
+
+    <link rel="stylesheet" href="./CSS/Page1.css">
+</head>
+<body>
+    <header>
+        <img class="Logo__Expo" src="./media/logo-expo.svg" alt="Logo Expo Ingenieria">
+        <img class="Logo__EscNegCie" src="./media/logotec-ings.svg" alt="Logo Escuela de Negocios">
+    </header>
+    <main>
+
+        <h2>Explora el futuro de la ingeniería</h2>
+
+        <div class="Usuarios">
+            <a href="./HTML/LoginUsuarios.html" rel="noopener noreferrer">USUARIOS</a>
+        </div>
+
+        <div class="Proyectos">
+            <a href="./HTML/LoginProyecto.html" rel="noopener noreferrer">PROYECTOS</a>
+        </div>
+        
+    </main>
+    <footer>
+        <h1>D.R. INSTITUTO TECNOLÓGICO Y DE ESTUDIOS SUPERIORES DE MONTERREY 2022.</h1>
+    </footer>
+</body>
+</html>
