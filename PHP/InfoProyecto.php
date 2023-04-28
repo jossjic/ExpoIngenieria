dde27dae86e7f79a83f73b3f352c25445513f675
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-28 00:36:18 -0600

    Logout - Categorias - Nivel

--- PHP/InfoProyecto.php
+++ PHP/InfoProyecto.php
@@ -1,6 +1,14 @@
 <?php 
     require_once 'dataBase.php';
 
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    } 
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		// Recuperamos los datos enviados por el formulario
 		$nombre_proyecto = $_POST['project_name'];
@@ -19,24 +27,27 @@
 				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
 				// Preparamos la consulta para insertar el proyecto
-				$stmt = $pdo->prepare("INSERT INTO PROYECTO(p_nombre, p_descripcion, ca_id, n_id) VALUES (:nombre, :descripcion, :categoria, :avance)");
+				$stmt = $pdo->prepare("INSERT INTO PROYECTO(p_nombre, p_descripcion, ca_id, n_id) VALUES (:nombre, :descripcion, :categoria, :avance) WHERE p_id = :id");
                 
                 echo $nombre_proyecto;
                 echo $descripcion_proyecto;
                 echo $categoria_proyecto;
                 echo $avance_proyecto;
+                echo $_SESSION['id'];
 
 				// Asignamos los valores a los parámetros de la consulta
 				$stmt->bindParam(':nombre', $nombre_proyecto);
 				$stmt->bindParam(':descripcion', $descripcion_proyecto);
 				$stmt->bindParam(':categoria', $categoria_proyecto);
 				$stmt->bindParam(':avance', $avance_proyecto);
+                $stmt->bindParam(':id', $_SESSION['id']);
 	
 				// Ejecutamos la consulta
 				$stmt->execute();
                 
 				// Mostramos un mensaje de éxito
 				echo 'El proyecto se ha guardado correctamente.';
+                
 			} catch (PDOException $e) {
 				// Mostramos un mensaje de error en caso de que ocurra un error al conectarnos a la base de datos
 				echo 'Error al conectarnos a la base de datos: ' . $e->getMessage();
