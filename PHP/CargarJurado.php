7b7420ecaea84ebfa68bce001489cb4f63cd89ee
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-28 16:58:49 -0600

    PHP Subir Jurado

--- PHP/CargarJurado.php
+++ PHP/CargarJurado.php
@@ -1,4 +1,6 @@
 <?php
+    require_once 'dataBase.php';
+
     if (isset($_FILES["file"])) {
         // Obtener la información del archivo
         $fileName = $_FILES["file"]["name"];
@@ -11,37 +13,62 @@
             $file = fopen($fileTmpName, "r");
             $headers = fgetcsv($file, 0, ",");
             while (($data = fgetcsv($file, 0, ",")) !== FALSE) {
-                // Insertar en la tabla COLABORADOR
+                //Conectar base de datos
+                $pdo = Database::connect();
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                // Verificar si el colaborador existe
                 $co_correo = $data[0];
-                $co_nomina = $data[1];
-                $co_nombre = $data[2];
-                $co_apellido = $data[3];
-                $co_pass = $data[4];
-                $co_es_jurado = ($data[5] === "1") ? 1 : 0;
-                
-                /*$sql = "INSERT INTO COLABORADOR (co_correo, co_nomina, co_nombre, co_apellido, co_pass, co_es_jurado)
-                        VALUES ('$co_correo', '$co_nomina', '$co_nombre', '$co_apellido', '$co_pass', '$co_es_jurado')";
-                $conn->query($sql);*/
-    
-                // Obtener el ID de la edición
-                $ed_nombre = $data[6];
-                /*$sql = "SELECT ed_id FROM EDICION WHERE ed_nombre = '$ed_nombre'";
-                $result = $conn->query($sql);
-                $row = $result->fetch_assoc();
-                $ed_id = $row["ed_id"];*/
-    
-                // Insertar en la tabla EDICION_COLABORADOR
-                /*$sql = "INSERT INTO EDICION_COLABORADOR (ed_id, co_correo)
-                        VALUES ('$ed_id', '$co_correo')";
-                $conn->query($sql);*/
+                $sql = "SELECT COUNT(*) FROM COLABORADOR WHERE co_correo = ?";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute([$co_correo]);
+                $exists = $stmt->fetchColumn();
+            
+                if (!$exists) {
+                    // Insertar en la tabla COLABORADOR
+                    $co_nomina = $data[1];
+                    $co_nombre = $data[2];
+                    $co_apellido = $data[3];
+                    $co_pass = $data[4];
+                    $co_es_jurado = ($data[5] === "1") ? 1 : 0;
+                    
+                    $sql = "INSERT INTO COLABORADOR (co_correo, co_nomina, co_nombre, co_apellido, co_pass, co_es_jurado) VALUES (?, ?, ?, ?, ?, ?)";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute([$co_correo, $co_nomina, $co_nombre, $co_apellido, $co_pass, $co_es_jurado]);
 
-                echo $co_correo. " " .$co_nomina. " " .$co_nombre. " " .$co_apellido. " " .$co_pass. " " .$co_es_jurado. " " .$ed_nombre ."\n";
+                    // Obtener el ID de la edición
+                    $ed_nombre = $data[6];
+                    $sql = "SELECT ed_id FROM EDICION WHERE ed_nombre = ?";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute([$ed_nombre]);
+                    $ed_id = $stmt->fetchColumn();
+                
+                    // Insertar en la tabla EDICION_COLABORADOR
+                    $sql = "INSERT INTO EDICION_COLABORADOR(co_corre,ed_id) VALUES (?, ?)";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute([$co_correo,$ed_id]);
+
+                } else {
+                    //Revisar que exista el colaborador en la tabla con la EDICION_COLABORADOR que se quiere
+                    $sql = "SELECT * FROM EDICION_COLABORADOR WHERE co_correo = ? AND ed_id = ?";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute([$co_correo,$ed_id]);
+                    $exists = $stmt->fetchColumn();
+
+                    if (!$exists) {
+                        $sql = "INSERT INTO EDICION_COLABORADOR(co_correo, ed_id) VALUES(?,?)";
+                        $stmt = $pdo->prepare($sql);
+                        $stmt->execute([$co_correo,$ed_id]);
+                    }
+                }
             }
+                    
             fclose($file);
     
             echo "Archivo cargado exitosamente.";
         } else {
             echo "El archivo debe ser un CSV.";
         }
+
     }
 ?>
