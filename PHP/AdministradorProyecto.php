6a9f30cd38fa3c106a386c6ac38bb0f7ac7da04c
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-28 00:30:16 -0600

    Logout - Categorias - Nivel

--- PHP/AdministradorProyecto.php
+++ PHP/AdministradorProyecto.php
@@ -34,47 +34,6 @@
 		Database::disconnect();
 	}
 
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		// Recuperamos los datos enviados por el formulario
-		$nombre_proyecto = $_POST['project_name'];
-		$descripcion_proyecto = $_POST['project_description'];
-		$categoria_proyecto = $_POST['category'];
-		$avance_proyecto = $_POST['level'];
-	
-		// Validamos que los campos no estén vacíos
-		if (empty($nombre_proyecto) || empty($descripcion_proyecto) || empty($categoria_proyecto) || empty($avance_proyecto)) {
-			// Mostramos un mensaje de error
-			echo 'Por favor, rellena todos los campos.';
-		} else {
-			// Realizamos la conexión a la base de datos con PDO
-			try {
-				$pdo = Database::connect();
-				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-				// Preparamos la consulta para insertar el proyecto
-				$stmt = $pdo->prepare("INSERT INTO proyectos (p_nombre, p_descripcion, ca_id, n_id) VALUES (:nombre, :descripcion, :categoria, :avance)");
-	
-				// Asignamos los valores a los parámetros de la consulta
-				$stmt->bindParam(':nombre', $nombre_proyecto);
-				$stmt->bindParam(':descripcion', $descripcion_proyecto);
-				$stmt->bindParam(':categoria', $categoria_proyecto);
-				$stmt->bindParam(':avance', $avance_proyecto);
-	
-				// Ejecutamos la consulta
-				$stmt->execute();
-	
-				// Mostramos un mensaje de éxito
-				echo 'El proyecto se ha guardado correctamente.';
-				header("Location: ../PHP/AdministradorProyecto.php");
-				exit();
-			} catch (PDOException $e) {
-				// Mostramos un mensaje de error en caso de que ocurra un error al conectarnos a la base de datos
-				echo 'Error al conectarnos a la base de datos: ' . $e->getMessage();
-			}
-		}
-	}
-
-
 
 ?>
 
@@ -204,7 +163,7 @@
 		</div>
 
 		<main>
-			<form class="First__Form" action="../PHP/AdministradorProyecto.php" method="post">
+			<form class="First__Form" action="../PHP/InfoProyecto.php" method="POST">
 				<table style="text-align: center">
 					<tr>
 						<th>
