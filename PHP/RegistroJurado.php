a4fb1e3d7f807a51eed7f29146d34d5ed79c4c59
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-28 13:39:03 -0600

    HTML- CSS Subir Jurado

---
+++ PHP/RegistroJurado.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Registro Jurado</title>
+
+		<link rel="stylesheet" href="../CSS/UploadJurado.css" />
+		<link rel="stylesheet" href="../CSS/HeaderFooterStructure.css" />
+	</head>
+	<body>
+		<header>
+			<img
+				class="Logo__EscNegCie"
+				src="/media/logotec-ings.svg"
+				alt="Logo Escuela de Negocios"
+			/>
+			<ul>
+				<li>
+					<a href="../" rel="noopener noreferrer">Inicio</a>
+				</li>
+			</ul>
+		</header>
+
+		<main>
+			<div class="upload">
+				<h2>Subir archivo CSV</h2>
+				<form
+					method="post"
+					action="CargarJurado.php"
+					enctype="multipart/form-data"
+				>
+					<input type="file" name="file" />
+					<button type="submit">Subir</button>
+				</form>
+				<br />
+				<br />
+				<button>Asignar Jurado Aleatoriamente</button>
+			</div>
+			<div class="container">
+				<h1>Jurado</h1>
+				<table>
+					<thead>
+						<tr>
+							<th>Nombre</th>
+							<th>Apellido</th>
+							<th>Edad</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
+			</div>
+		</main>
+	</body>
+</html>
