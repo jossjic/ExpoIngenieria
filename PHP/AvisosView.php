d3e5fc387a9fec2c10b8653b9ee2c95439a36f45
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-29 14:50:17 -0600

    Avisos View Ready - [Flag = 1]

--- PHP/AvisosView.php
+++ PHP/AvisosView.php
@@ -1,5 +1,15 @@
 <?php 
 	require 'dataBase.php';
+
+	require_once 'dataBase.php';
+
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +19,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="icon" type="image/ico" href="../media/favicon.ico"/>
 
-        <title>Anuncios View</title>
+        <title>Avisos View</title>
 
         <link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
         <link rel="stylesheet" href="../CSS/AdminPages.css">
@@ -18,18 +28,26 @@
 
     <body>
 		<header>
-			<img class="Logo__EscNegCie" src="../media/logotec-ings.svg" alt="Logo__EscNegCie">
-			<ul>
-					<li><a href="#">Menu</a></li>
-					<li><a href="#">Usuarios</a></li>
-					<li><a href="#">Reconocimientos</a></li>
-					<li><a href="#">Eastadísticas</a></li>
+			<a href="../index.php"
+				><img
+					class="Logo__Expo"
+					src="../media/logo-expo.svg"
+					alt="Logotipo de Expo ingenierías"
+			/></a>
+			<ul style="grid-column: 2/4">
+				<li><a href="#">Menu</a></li>
+				<li><a href="#">Avisos</a></li>
+				<li><a href="#">Ediciones</a></li>
+				<li><a href="#">Nivel</a></li>
+				<li><a href="#">Categorias</a></li>
+				<li><a href="#">Usuarios</a></li>
+				<li><a href="#">Proyectos</a></li>
+				<li><a href="#">Administradores</a></li>
+				<li><a href="#">Evaluaciones</a></li>
+				<li style="font-weight: 600; font-size: 1.2em">
+					<a href="#">Cerrar Sesion</a>
+				</li>
 			</ul>
-			<nav>
-				<ul>
-					<li><a href="#">Cerrar Sesion</a></li>
-				</ul>
-			</nav>
 		</header>
 
 		<main>
@@ -67,7 +85,8 @@
 						<td>
 							<select name="Anuncio" id="Anuncio">
 								<option value="Titulo">Titulo</option>
-								<option value="Administrador">Administrador</option>
+								<option value="Grupo">Grupo</option>
+								<option value="Fecha">Fecha</option>
 							</select>
 						</td>
 						<td>
@@ -86,9 +105,9 @@
 					<p>&nbsp;</p>
 					<p></p>
 					<p>ID</p>
-					<p>Nombre</p>
-					<p>Administrador</p>
-					<p></p>
+					<p>Titulo</p>
+					<p>Grupo</p>
+					<p>Fecha</p>
 					<div>
 						<p>Acciones</p>
 					</div>
@@ -103,9 +122,9 @@
 											<p>&nbsp;</p>
                                             <p></p>
                                             <p>ID</p>
-                                            <p>Nombre</p>
-                                            <p>Administrador</p>
-                                            <p></p>
+                                            <p>Titulo</p>
+                                            <p>Grupo</p>
+                                            <p>Fecha</p>
                                             <div class='Btn__Green'>
                                                 <a href='../PHP/AvisosRead.php?id=".trim($row['an_id'])."'>Ver</a>
                                             </div>
