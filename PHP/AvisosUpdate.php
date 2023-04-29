24d36219e0d9198a73d57f7a532d5b58fd9246ea
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-29 15:49:56 -0600

    Avisos Update Ready - [Flag = 1]

--- PHP/AvisosUpdate.php
+++ PHP/AvisosUpdate.php
@@ -1,6 +1,14 @@
 <?php
 
-	require 'dataBase.php';
+    require_once 'dataBase.php';
+
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    }
 
 	$id = null;
 	if ( !empty($_GET['id'])) {
@@ -98,31 +106,26 @@
     <body>
         
         <header>
-			<img class="Logo__EscNegCie" src="../media/logotec-ings.svg" alt="Logo__EscNegCie">
-
-            <ul>
-
-                <li>
-                    <a href="#">Menu</a>
-                </li>
-				<li>
-                    <a href="#">Usuarios</a>
-                </li>
-				<li>
-                    <a href="#">Reconocimientos</a>
-                </li>
-				<li>
-                    <a href="#">Eastadísticas</a>
-                </li>
-				
+			<a href="../index.php"
+				><img
+					class="Logo__Expo"
+					src="../media/logo-expo.svg"
+					alt="Logotipo de Expo ingenierías"
+			/></a>
+			<ul style="grid-column: 2/4">
+				<li><a href="../PHP/AdminInicio.php">Menu</a></li>
+				<li><a href="../PHP/AvisosView.php">Avisos</a></li>
+				<li><a href="../PHP/EdicionView.php">Ediciones</a></li>
+				<li><a href="../PHP/NivelView.php">Nivel</a></li>
+				<li><a href="../PHP/CategoriasView.php">Categorias</a></li>
+				<li><a href="../PHP/UsuariosView.php">Usuarios</a></li>
+				<li><a href="../PHP/ProyectosView.php">Proyectos</a></li>
+				<li><a href="../PHP/AdministradoresView.php">Administradores</a></li>
+				<li><a href="../PHP/EvaluacionesView.php">Evaluaciones</a></li>
+				<li style="font-weight: 600; font-size: 1.2em">
+					<a href="../PHP/logout.php">Cerrar Sesion</a>
+				</li>
 			</ul>
-
-            <nav>
-				<ul>
-					<li><a href="#">Cerrar Sesion</a></li>
-				</ul>
-			</nav>
-
 		</header>
 
         <main>
@@ -175,18 +178,6 @@
                             <input class="Text__Input" name="Grupo" type="text"  placeholder="" value="<?php echo !empty($Grupo)?$Grupo:'';?>">
                             <?php if (($GrupoError != null)) ?>
                             <span class="help-inline"><?php echo $GrupoError;?></span>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <label>Fecha</label>
-                        </td>
-
-                        <td>
-                            <input class="Text__Input" name="Fecha" type="date" readonly placeholder="" value="<?php echo !empty($Fecha)?$Fecha:'';?>">
-                            <?php if (($FechaError != null)) ?>
-                            <span class="help-inline"><?php echo $FechaError;?></span>
                         </td>
                     </tr>
 
