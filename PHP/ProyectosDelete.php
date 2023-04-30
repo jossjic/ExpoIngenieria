8c0ac838f4b9bb26e714df9be1a7f3471da4d252
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-30 00:46:03 -0600

    Proyectos Delete Ready - [Flag = 1]

--- PHP/ProyectosDelete.php
+++ PHP/ProyectosDelete.php
@@ -16,7 +16,7 @@
 
     $pdo = Database::connect();
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "SELECT FROM PROYECTO WHERE p_id = ?";
+	$sql = "SELECT * FROM PROYECTO WHERE p_id = ?";
 	$q = $pdo->prepare($sql);
 	$q->execute(array($id));
     $data = $q->fetch(PDO::FETCH_ASSOC);
