f6446fb4bccbc2a55fc3c11239127abfdf84510b
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-30 00:11:54 -0600

    Proyectos Read Ready - [Flag = 1]

--- PHP/ProyectosRead.php
+++ PHP/ProyectosRead.php
@@ -1,5 +1,13 @@
 <?php
-	require 'dataBase.php';
+	require_once 'dataBase.php';
+
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    }
 
 	$id = null;
 	if ( !empty($_GET['id'])) {
@@ -11,7 +19,12 @@
 	} else {
 		$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "SELECT * FROM PROYECTOV1 WHERE p_id = ?";
+		$sql = "SELECT * 
+                FROM PROYECTO 
+                NATURAL JOIN EDICION
+                NATURAL JOIN NIVEL
+                NATURAL JOIN CATEGORIA
+                WHERE p_id = ?";
 		$q = $pdo->prepare($sql);
 		$q->execute(array($id));
 		$data = $q->fetch(PDO::FETCH_ASSOC);
@@ -27,167 +40,121 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/ico" href="../media/favicon.ico"/>
 
-    <title>Proyectos View</title>
+    <title>Ver Proyectos</title>
 
     <link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
-    <link rel="stylesheet" href="../CSS/AdminPages.css">
+    <link rel="stylesheet" href="../CSS/FormsStructure.css">
+    <link rel="stylesheet" href="../CSS/Extra.css">
 
 </head>
 <body>
 
-    <header>
-        <img class="Logo__EscNegCie" src="../media/logotec-ings.svg" alt="Logo__EscNegCie">
-        <ul>
-            <li>
-                <a href="#">Cerrar Sesion</a>
-            </li>
-        </ul>
-        <nav>
-            <ul>
-                <li><a href="#">Menu</a></li>
-                <li><a href="#">Usuarios</a></li>
-                <li><a href="#">Reconocimientos</a></li>
-                <li><a href="#">Eastadísticas</a></li>
-            </ul>
-        </nav>
-    </header>
+        <header>
+			<a href="../index.php"
+				><img
+					class="Logo__Expo"
+					src="../media/logo-expo.svg"
+					alt="Logotipo de Expo ingenierías"
+			/></a>
+			<ul style="grid-column: 2/4">
+				<li><a href="../PHP/AdminInicio.php">Menu</a></li>
+				<li><a href="../PHP/AvisosView.php">Avisos</a></li>
+				<li><a href="../PHP/EdicionView.php">Ediciones</a></li>
+				<li><a href="../PHP/NivelView.php">Nivel</a></li>
+				<li><a href="../PHP/CategoriasView.php">Categorias</a></li>
+				<li><a href="../PHP/UsuariosView.php">Usuarios</a></li>
+				<li><a href="../PHP/ProyectosView.php">Proyectos</a></li>
+				<li><a href="../PHP/AdministradoresView.php">Administradores</a></li>
+				<li><a href="../PHP/EvaluacionesView.php">Evaluaciones</a></li>
+				<li style="font-weight: 600;">
+					<a href="../PHP/logout.php">Cerrar Sesion</a>
+				</li>
+			</ul>
+		</header>
 
     <main>
 
-        <div class="Admin__Start">
-            <div class="Admin__Start__Left">
-                <h1>Administrador de Proyectos</h1>
-                <table>
-                    <tr>
-                        <td>Total de Proyectos:</td>
-                        <td id="TotalProyectos">
-                            <?php
-                                $pdo = Database::connect();
-                                $sql = "SELECT * FROM PROYECTOV1";
-                                $q = $pdo->query($sql)->rowCount();
-                                echo "$q";
-                                Database::disconnect();
-                            ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Total Calificados:</td>
-                        <td id="TotalCalificados">
-                            <?php
-                                $pdo = Database::connect();
-                                $sql = "SELECT * FROM PROYECTOV1";
-                                $q = $pdo->query($sql)->rowCount();
-                                echo "$q";
-                                Database::disconnect();
-                            ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Total Pendientes:</td>
-                        <td id="TotalPendientes">
-                            <?php
-                                $pdo = Database::connect();
-                                $sql = "SELECT * FROM PROYECTOV1";
-                                $q = $pdo->query($sql)->rowCount();
-                                echo "$q";
-                                Database::disconnect();
-                            ?>
-                        </td>
-                    </tr>
-                </table>
-            </div>
+        <h1>Detalles del proyecto</h1>
 
-            <div class="Estadisticas__Btn">
-                <a class="Admin__Start__Right__Btn" href="../PHP/EstadisticasUsuarios.php">Estadisticas Proyectos</a>
-            </div>
-        </div>
+        <form>
 
-        <form method="post" class="Winners__Explorer">
             <table>
                 <tr>
                     <td>
-                        Buscar
+                        <label for="">Nombre</label>
                     </td>
                     <td>
-                        <select name="ProyectoID" id="ProyectoID">
-                            <option value="ID">ID</option>
-                            <option value="Nombre">Nombre</option>
-                        </select>
+                        <?php echo $data['p_nombre'];?>
                     </td>
-                    <td>
-                        
-                        <input type="search" name="BuscarNombre" class="Text__Search" id="" placeholder="Ingresa el valor">
-                        <input type="submit" name="BtnBuscar" class="Search__Btn" id="" value="Buscar">
-                        
-                    </td>
-                    
                 </tr>
-              </table>
+
+                <tr>
+                    <td>
+                        <label for="">Descripcion</label>
+                    </td>
+                    <td>
+                        <?php echo $data['p_descripcion'];?>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label for="">Video</label>
+                    </td>
+                    <td>
+                        <?php echo $data['p_nombre'];?>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label for="">Poster</label>
+                    </td>
+                    <td>
+                        <?php echo $data['p_nombre'];?>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label for="">Nivel</label>
+                    </td>
+                    <td>
+                        <?php echo $data['n_nombre'];?>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label for="">Categoria</label>
+                    </td>
+                    <td>
+                        <?php echo $data['ca_nombre'];?>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label for="">Estado</label>
+                    </td>
+                    <td>
+                        <?php echo $data['p_estado'];?>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label for="">Edicion</label>
+                    </td>
+                    <td>
+                        <?php echo $data['ed_nombre'];?>
+                    </td>
+                </tr>
+
+            </table>
+
         </form>
 
-        <div class="Info">
-
-            <div class="Name__Read">
-                <?php
-                    echo "<h1>" . $data['p_nombre'] ."</h1>";
-                ?>
-            </div>
-            
-            <div class="Info__Read">
-
-                <div class="InfoRead__Atributes">
-                    <?php
-                        echo "<p>" . $data['p_id'] ."</p>";
-                    ?>
-                </div>
-
-                <div class="InfoRead__Atributes">
-                    <?php
-                        echo "<p>" . $data['p_nombre'] ."</p>";
-                    ?>
-                </div>
-
-                <div class="InfoRead__Atributes">
-                    <?php
-                        echo "<p>" . $data['p_descripcion'] ."</p>";
-                    ?>
-                </div>
-
-                <div class="InfoRead__Atributes">
-                    <?php
-                        echo "
-                                <p>" . $data['p_categoria'] ."</p>
-                                <p>" . $data['p_estado'] ."</p>
-                             ";
-                    ?>
-                </div>
-
-                <div class="InfoRead__Atributes">
-                    <?php
-                        echo "
-                                <p>" . $data['a_nombre'] ."</p>
-                                <p>" . $data['a_matricula'] ."</p>
-                             ";
-                    ?>
-                </div>
-                <div class="InfoRead__Atributes">
-                    <?php
-                        echo "
-                                <p>" . $data['a_nombre'] ."</p>
-                                <p>" . $data['a_matricula'] ."</p>
-                             ";
-                    ?>
-                </div>
-                <div class="InfoRead__Atributes">
-                    <?php
-                        echo "
-                                <p>" . $data['a_nombre'] ."</p>
-                                <p>" . $data['a_matricula'] ."</p>
-                             ";
-                    ?>
-                </div>
-            </div>
-        </div>
     </main> 
 
 </body>
