aabedaf7b7eba5343fdd007bf98e75d6e7b7def8
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-30 19:19:33 -0600

    Nivel Read Ready - [Flag=1]

--- PHP/NivelRead.php
+++ PHP/NivelRead.php
@@ -1,11 +1,21 @@
 <?php
-	require 'dataBase.php';
+	require_once 'dataBase.php';
+
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    }
+
 	$id = null;
 	if ( !empty($_GET['id'])) {
 		$id = $_REQUEST['id'];
 	}
 	if ( $id==null) {
-		header("Location: index.php");
+		header("Location: ../index.php");
+		exit();
 	} else {
 		$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -23,62 +33,72 @@
     <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>READ EDICION</title>
+        <title>Ver Edicion</title>
 
         <link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
-        <link rel="stylesheet" href="../CSS/galeria.css">
+        <link rel="stylesheet" href="../CSS/FormsStructure.css">
+        <link rel="stylesheet" href="../CSS/Extra.css">
+
 	</head>
 
-    <header>
-        <img class="Logo__EscNegCie" src="../media/logotec-ings.svg" alt="Logo__EscNegCie">
-        <ul>
-        <li>
-            <a href="#">Layout Proyectos</a>
-        </li>
-        </ul>
-        <nav>
-        <ul>
-            <li><a href="#">Cerrar Sesión</a></li>
-        </ul>
-        </nav>
-    </header>
-    <br>
-    <br>
 	<body>
-    	<div class="container">
+		
+		<header>
+			<a href="../index.php"
+				><img
+					class="Logo__Expo"
+					src="../media/logo-expo.svg"
+					alt="Logotipo de Expo ingenierías"
+			/></a>
+			<ul style="grid-column: 2/4">
+				<li><a href="../PHP/AdminInicio.php">Menu</a></li>
+				<li><a href="../PHP/AvisosView.php">Avisos</a></li>
+				<li><a href="../PHP/EdicionView.php">Ediciones</a></li>
+				<li><a href="../PHP/NivelView.php">Nivel</a></li>
+				<li><a href="../PHP/CategoriasView.php">Categorias</a></li>
+				<li><a href="../PHP/UsuariosView.php">Usuarios</a></li>
+				<li><a href="../PHP/ProyectosView.php">Proyectos</a></li>
+				<li><a href="../PHP/AdministradoresView.php">Administradores</a></li>
+				<li><a href="../PHP/EvaluacionesView.php">Evaluaciones</a></li>
+				<li style="font-weight: 600;">
+					<a href="../PHP/logout.php">Cerrar Sesion</a>
+				</li>
+			</ul>
+		</header>
 
-    		<div class="span10 offset1">
-    			<div class="row">
-		    		<h3>Detalles de Nivel</h3>
-		    	</div>
-                <br>
-	    		<div class="form-horizontal" >
+		<main>
 
-					<div class="control-group">
-						<label class="control-label">ID</label>
-					    <div class="controls">
-							<label class="checkbox">
-								<?php echo $data['n_id'];?>
-							</label>
-					    </div>
-					</div>
+			<h1>Detalles del Nivel</h1>
 
-					<div class="control-group">
-					    <label class="control-label">Nombre</label>
-					    <div class="controls">
-					      	<label class="checkbox">
-						     	<?php echo $data['n_nombre'];?>
-						    </label>
-					    </div>
-					</div>
+			<form>
+                <table>
+                    <tr>
+                        <td>
+                            <label for="">ID</label>
+                        </td>
+                        <td>
+                            <?php echo $data['n_id'];?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="">Nombre</label>
+                        </td>
+                        <td>
+                            <?php echo $data['n_nombre'];?>
+                        </td>
+                    </tr>
 
-                    <br>
-				    <div class="form-actions">
-						<a class="btn" href="NivelCRUD.php">Regresar</a>
-					</div>
+                    <tr>
+                        <td><a class="Btn-Ancla" href="NivelView.php">Regresar</a></td>
+                        <td></td>
+                    </tr>
+                </table>
 
-				</div>
-			</div>
-		</div> <!-- /container -->
-  	</body>
+            </form>
+
+		</main>
+
+	</body>
+	
 </html>
