0b4ccd897b16c05fc62b63ad924e4a638db738e9
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-30 18:25:26 -0600

    Categoria Delete Ready - [Flag=1]

--- PHP/CategoriaDelete.php
+++ PHP/CategoriaDelete.php
@@ -1,5 +1,14 @@
 <?php
-	require 'dataBase.php';
+	require_once 'dataBase.php';
+
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    }
+
 	$id = 0;
 	if ( !empty($_GET['id'])) {
 		$id = $_REQUEST['id'];
@@ -16,51 +25,86 @@
 		$q->execute(array($id));
 		Database::disconnect();
 		header("Location: CategoriaCRUD.php");
+		exit();
 	}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-    <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>DELETE CATEGORIA</title>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Eliminar Categoria</title>
 
-        <link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
-        <link rel="stylesheet" href="../CSS/galeria.css">
+		<link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
+		<link rel="stylesheet" href="../CSS/FormsStructure.css">
+		<link rel="stylesheet" href="../CSS/Extra.css">
+
 	</head>
 
-    <header>
-        <img class="Logo__EscNegCie" src="../media/logotec-ings.svg" alt="Logo__EscNegCie">
-        <ul>
-        <li>
-            <a href="#">Layout Proyectos</a>
-        </li>
-        </ul>
-        <nav>
-        <ul>
-            <li><a href="#">Cerrar Sesión</a></li>
-        </ul>
-        </nav>
-    </header>
-
 	<body>
-	    <div class="container">
-	    	<div class="span10 offset1">
-	    		<div class="row">
-			    	<h3>Eliminar una categoria</h3>
-			    </div>
 
-			    <form class="form-horizontal" action="deleteCategoria.php" method="post">
-		    		<input type="hidden" name="id" value="<?php echo $id;?>"/>
-					<p class="alert alert-error">Estas seguro que quieres eliminar esta categoria ?</p>
-					<div class="form-actions">
-						<button type="submit" class="btn btn-danger">Si</button>
-						<a class="btn" href="CategoriaCRUD.php">No</a>
-					</div>
-				</form>
-			</div>
-	  </div> <!-- /container -->
+		<header>
+			<a href="../index.php"
+				><img
+					class="Logo__Expo"
+					src="../media/logo-expo.svg"
+					alt="Logotipo de Expo ingenierías"
+			/></a>
+			<ul style="grid-column: 2/4">
+				<li><a href="../PHP/AdminInicio.php">Menu</a></li>
+				<li><a href="../PHP/AvisosView.php">Avisos</a></li>
+				<li><a href="../PHP/EdicionView.php">Ediciones</a></li>
+				<li><a href="../PHP/NivelView.php">Nivel</a></li>
+				<li><a href="../PHP/CategoriasView.php">Categorias</a></li>
+				<li><a href="../PHP/UsuariosView.php">Usuarios</a></li>
+				<li><a href="../PHP/ProyectosView.php">Proyectos</a></li>
+				<li><a href="../PHP/AdministradoresView.php">Administradores</a></li>
+				<li><a href="../PHP/EvaluacionesView.php">Evaluaciones</a></li>
+				<li style="font-weight: 600">
+					<a href="../PHP/logout.php">Cerrar Sesion</a>
+				</li>
+			</ul>
+		</header>
+
+		<main>
+            <h1>Eliminar Categoria</h1>
+
+            <form action="../PHP/CategoriaDelete.php" method="post">
+
+                <table>
+                    <tr>
+                        <td>
+                            <input type="hidden" name="id" value="<?php echo $id;?>"/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="text-align: center;">
+                            <p>Estas seguro de eliminar esta categoria</p>
+                            <p>No lo podras recuperar</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td class="Btn-Ancla">
+                            <input class="Btn__Iniciar__Sesion" type="submit" value="Si" id="submit" name="submit">
+                        </td>
+                    </tr>
+
+					<tr>
+						<td>
+                            <a class="Btn-Ancla" href="CategoriaView.php">Regresar</a>
+						</td>
+					</tr>
+                </table>
+
+            </form>
+
+        </main>
+
 	</body>
+
 </html>
+
