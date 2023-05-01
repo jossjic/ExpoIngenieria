a4a6ca3e196032bcf0080595e682828753e55076
Josafat <josafatgarciasarmientos@gmail.com>
2023-05-01 01:01:35 -0600

    Usuarios View Ready - [Flag = 1]

--- PHP/UsuariosView.php
+++ PHP/UsuariosView.php
@@ -1,38 +1,55 @@
-<?php
-    require 'dataBase.php'
+<?php 
+
+	require_once 'dataBase.php';
+
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin Usuarios</title>
-  <link rel="icon" type="image/ico" href="../media/favicon.ico"/>
+  <title>Usuarios View</title>
+
   <link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
   <link rel="stylesheet" href="../CSS/AdminPages.css">
+  <link rel="stylesheet" href="../CSS/Extra.css">
 </head>
 <body>
 
-  <header>
-      <img class="Logo__EscNegCie" src="../media/logo-expo.svg" alt="Logo__EscNegCie">
-      <ul>
-          <li>
-              <a href="#">Cerrar Sesion</a>
-          </li>
-      </ul>
-      <nav>
-          <ul>
-          <li><a href="../PHP/ProyectosView.php">Proyectos</a></li>
-              <li><a href="../PHP/UsuariosView.php">Usuarios</a></li>
-              <li><a href="../PHP/ReconocimientosView.php">Reconocimientos</a></li>
-              <li><a href="../PHP/EstadisticasView.php">Estadísticas</a></li>
-          </ul>
-      </nav>
-  </header>
+        <header>
+			<a href="../index.php"
+				><img
+					class="Logo__Expo"
+					src="../media/logo-expo.svg"
+					alt="Logotipo de Expo ingenierías"
+			/></a>
+			<ul style="grid-column: 2/4">
+				<li><a href="../PHP/AdminInicio.php">Menu</a></li>
+				<li><a href="../PHP/AvisosView.php">Avisos</a></li>
+				<li><a href="../PHP/EdicionView.php">Ediciones</a></li>
+				<li><a href="../PHP/NivelView.php">Nivel</a></li>
+				<li><a href="../PHP/CategoriaView.php">Categorias</a></li>
+				<li><a href="../PHP/UsuariosView.php">Usuarios</a></li>
+				<li><a href="../PHP/ProyectosView.php">Proyectos</a></li>
+				<li><a href="../PHP/AdministradoresView.php">Administradores</a></li>
+				<li><a href="../PHP/EvaluacionesView.php">Evaluaciones</a></li>
+				<li style="font-weight: 600;">
+					<a href="../PHP/logout.php">Cerrar Sesion</a>
+				</li>
+			</ul>
+		</header>
 
-  <main>
-        <div class="Admin__Start">
+  <main class="Size">
+        <div class="Admin__Start ">
             <div class="Admin__Start__Left">
                 <h1>Administrador de Usuarios</h1>
                 <table>
@@ -45,9 +62,7 @@
                                 $q1 = $pdo->query($sql)->rowCount();
                                 $sql = "SELECT * FROM ALUMNO";
                                 $q2 = $pdo->query($sql)->rowCount();
-                                $sql = "SELECT * FROM ADMIN";
-                                $q3 = $pdo->query($sql)->rowCount();
-                                $a=$q1+$q2+$q3;
+                                $a=$q1+$q2;
                                 echo "$a";
                                 Database::disconnect();
                             ?>
@@ -66,7 +81,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>Profesores:</td>
+                        <td>Docentes: </td>
                         <td id="TotalProfes">
                         <?php
                                 $pdo = Database::connect();
@@ -89,20 +104,10 @@
                             ?>
                         </td>
                     </tr>
-                    <tr>
-                        <td>Admins:</td>
-                        <td id="TotalAdmins">
-                        <?php
-                                $pdo = Database::connect();
-                                $sql = "SELECT * FROM ADMIN";
-                                $q = $pdo->query($sql)->rowCount();
-                                echo "$q";
-                                Database::disconnect();
-                            ?>
-                        </td>
-                    </tr>
                 </table>
             </div>
+
+        </div>
 
             <div class="Estadisticas__Btn">
                 <a cglass="Admin__Start__Right__Btn" href="../PHP/EstadisticasUsuarios.php">Estadisticas Usuarios</a>
@@ -117,7 +122,7 @@
                     </td>
                     <td>
                         <select name="UserID" id="UserID">
-                            <option value="Nomina/Matrícula">Nomina/Matrícula</option>
+                            <option value="Matrícula">Matricula</option>
                             <option value="Tipo">Tipo</option>
                             <option value="Nombre">Nombre</option>
                             <option value="Apellido">Apellido</option>
@@ -153,13 +158,12 @@
 
         <div class="Info">
             <div class="Info__Header">
-               <p>&nbsp;</p>
-                <!-- <p></p> -->
-                <p>Nomina/Matrícula</p>
+                <p>&nbsp;</p>
+                <p></p>
+                <p>Matrícula</p>
                 <p>Tipo</p>
                 <p>Nombre</p>
                 <p>Apellido</p>
-                <p>Correo</p>
                 <p>Acciones</p>
              </div>
                 <div class="Info__Table">
@@ -169,6 +173,7 @@
 								   	$sql = 'SELECT * FROM COLABORADOR ORDER BY co_apellido';
 				 				   	foreach ($pdo->query($sql) as $row) {
                                         /*echo '<input type="checkbox" name="" id="">' ;*/
+                                        echo '<p></p>';
                                         echo '<p></p>';
 
                                     
@@ -184,9 +189,10 @@
                                         else {
                                             echo '<p>N/A</p>';
                                             echo '<p>Externo</p>';}
+
                                             echo '<p>'. $row['co_nombre'] . '</>';
                                             echo '<p>'. $row['co_apellido'] . '</p>';
-                                            echo '<p>'. $row['co_correo'] . '</p>';
+                                            
                                 
                                         echo '<p></p>';
 
@@ -204,38 +210,18 @@
 				 				   	foreach ($pdo->query($sql) as $row) {
                                        /* echo '<input type="checkbox" name="" id="">' ;*/
                                         echo '<p></p>';
+                                        echo '<p></p>';
 
                                         echo '<p>'. $row['a_matricula'] . '</p>';
                                         echo '<p>Alumno</p>';
 			    					   	echo '<p>'. $row['a_nombre'] . '</>';
 			    					  	echo '<p>'. $row['a_apellido'] . '</p>';
-                      					echo '<p>'. $row['a_correo'] . '</p>';
+                      					
                                         echo '<p></p>';
 
 			    					   	echo ' <div class="Btn__Green"> <a href="UsuariosRead.php?correo='.$row['a_correo'].'&type=al">Ver</a></div>';
 			    					  	echo ' <div class="Btn__Blue"> <a href="UsuariosUpdate.php?correo='.$row['a_correo'].'&type=al">Actualizar</a></div>';
 			    					   	echo ' <div class="Btn__Red" ><a href="UsuariosDelete.php?correo='.$row['a_correo'].'&type=al">Eliminar</a></div>';
-                                        
-								    }
-								   	Database::disconnect();
-				  				?>
-                                <?php
-
-								   
-								   	$sql = 'SELECT * FROM ADMIN ORDER BY adm_apellido';
-				 				   	foreach ($pdo->query($sql) as $row) {
-                                       /* echo '<input type="checkbox" name="" id="">' ;*/
-                                        echo '<p></p>';
-                                        echo '<p>N/A</p>';
-                                        echo '<p>Administrador</p>';
-			    					   	echo '<p>'. $row['adm_nombre'] . '</>';
-			    					  	echo '<p>'. $row['adm_apellido'] . '</p>';
-                      					echo '<p>'. $row['adm_correo'] . '</p>';
-                                        echo '<p></p>';
-
-			    					   	echo ' <div class="Btn__Green"> <a href="UsuariosRead.php?correo='.$row['adm_correo'].'&type=adm">Ver</a></div>';
-			    					  	echo ' <div class="Btn__Blue"> <a href="UsuariosUpdate.php?correo='.$row['adm_correo'].'&type=adm">Actualizar</a></div>';
-			    					   	echo ' <div class="Btn__Red" ><a href="UsuariosDelete.php?correo='.$row['adm_correo'].'&type=adm">Eliminar</a></div>';
                                         
 								    }
 								   	Database::disconnect();
