452e9214776da3937f079075ff856dc6dbe80ce1
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-30 19:07:22 -0600

    Nivel View Ready - [Flag=1]

--- PHP/NivelView.php
+++ PHP/NivelView.php
@@ -1,88 +1,148 @@
+<?php 
+
+	require_once 'dataBase.php';
+
+    session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+        header("Location: ../index.php");
+        exit();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>CRUD NIVEL</title>
+        <title>Niveles View</title>
 
         <link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
-        <link rel="stylesheet" href="../CSS/galeria.css">
+        <link rel="stylesheet" href="../CSS/AdminPages.css">
 	</head>
 
-
-    <header>
-    <img class="Logo__EscNegCie" src="../media/logotec-ings.svg" alt="Logo__EscNegCie">
-    <ul>
-      <li>
-        <a href="#">Layout Proyectos</a>
-      </li>
-    </ul>
-    <nav>
-      <ul>
-        <li><a href="#">Cerrar Sesión</a></li>
-      </ul>
-    </nav>
-  </header>
-    <br>
-    <br>
-    <br>
 	<body>
-	    <div class="proyecto">
+		
+		<header>
+			<a href="../index.php"
+				><img
+					class="Logo__Expo"
+					src="../media/logo-expo.svg"
+					alt="Logotipo de Expo ingenierías"
+			/></a>
+			<ul style="grid-column: 2/4">
+				<li><a href="../PHP/AdminInicio.php">Menu</a></li>
+				<li><a href="../PHP/AvisosView.php">Avisos</a></li>
+				<li><a href="../PHP/EdicionView.php">Ediciones</a></li>
+				<li><a href="../PHP/NivelView.php">Nivel</a></li>
+				<li><a href="../PHP/CategoriasView.php">Categorias</a></li>
+				<li><a href="../PHP/UsuariosView.php">Usuarios</a></li>
+				<li><a href="../PHP/ProyectosView.php">Proyectos</a></li>
+				<li><a href="../PHP/AdministradoresView.php">Administradores</a></li>
+				<li><a href="../PHP/EvaluacionesView.php">Evaluaciones</a></li>
+				<li style="font-weight: 600;">
+					<a href="../PHP/logout.php">Cerrar Sesion</a>
+				</li>
+			</ul>
+		</header>
 
-    		<div class="row">
-    			<h3>Nivel Expo Ingenierias</h3>
-    		</div>
+		<main>
 
-				<div class="row">
-					<p>
-						<a href="createNivel.php" class="btn btn-success">Agregar un nivel</a>
-					</p>
+			<div class="Admin__Start">
+				<div class="Admin__Start__Left">
+					<h1>Administrador de Anuncios</h1>
+					<table>
+						<tr>
+							<td>Niveles Totales: </td>
+							<td id="TotalNiveles">
+								<?php
+									$pdo = Database::connect();
+									$sql = "SELECT * FROM NIVEL";
+									$q = $pdo->query($sql)->rowCount();
+									echo $q;
+									Database::disconnect();
+								?>
+							</td>
+						</tr>
+					</table>
+				</div>
 
-				<table class="table table-striped table-bordered">
-	            <thead>
-	                <tr>
-	                	<th >ID	</th>
-	                	<th>NOMBRE				</th>
-	                  <!--<th>A/C 					</th> -->
-	                </tr>
-	            </thead>
-                <br>
-                <br>
-	            <tbody>
-	              	<?php
-								   	include 'database.php';
-								   	$pdo = Database::connect();
-								   	$sql = 'SELECT * FROM NIVEL ORDER BY n_nombre';
-				 				   	foreach ($pdo->query($sql) as $row) {
-											echo '<tr>';
-			    					   	echo '<td>'. $row['n_id'] . '</td>';
-			    					  	echo '<td>'. $row['n_nombre'] . '</td>';
-			                            //echo '<td>';    echo ($row['ac'])?"SI":"NO"; echo'</td>';
-			                            echo '<td width=250>';
-			    					   	echo '<a class="btn" href="readNivel.php?id='.$row['n_id'].'">Detalles</a>';
-			    					   	echo '&nbsp;';
-			    					  	echo '<a class="btn btn-success" href="updateNivel.php?id='.$row['n_id'].'">Actualizar</a>';
-			    					   	echo '&nbsp;';
-			    					   	echo '<a class="btn btn-danger" href="deleteNivel.php?id='.$row['n_id'].'">Eliminar</a>';
-			    					   	echo '</td>';
-										  echo '</tr>';
-								    }
-								   	Database::disconnect();
-				  				?>
-			    		</tbody>
-		      </table>
+				<div class="Estadisticas__Btn">
+					<a class="Admin__Start__Right__Btn" href="../PHP/NivelCreate.php">Crear Nivel</a>
+				</div>
+			</div>
 
-		    </div>
+			<form action="../PHP/NivelBusqueda.php" method="post" class="Winners__Explorer">
+				<table>
+					<tr>
+						<td>
+							Buscar
+						</td>
+						<td>
+							<select name="Nivel" id="Anuncio">
+								<option value="Nombre">Nombre</option>
+							</select>
+						</td>
+						<td>
+							
+							<input type="search" name="BuscarNombre" class="Text__Search" id="" placeholder="Ingresa el valor">
+							<input type="submit" name="BtnBuscar" class="Search__Btn" id="" value="Buscar">
+							
+						</td>
+						
+					</tr>
+				</table>
+			</form>
 
-	    </div> <!-- /container -->
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <footer>
-        <img class="Logo__Tec" src="../media/LogoTec.png" alt="Logo TEC">
-        </footer>
+			<form method="post" class="Info">
+				<div class="Info__Header">
+					<p>&nbsp;</p>
+					<p></p>
+					<p>ID</p>
+					<p>Nombre</p>
+					<p></p>
+					<p></p>
+					<div>
+						<p>Acciones</p>
+					</div>
+				</div>
+				<div class="Info__Table">
+							
+                                <?php
+									$pdo = Database::connect();
+									$sql = 'SELECT * FROM NIVEL ORDER BY n_nombre';
+									foreach ($pdo->query($sql) as $row) {
+										echo "
+											<p>&nbsp;</p>
+                                            <p></p>
+                                            <p>".$row['n_id']."</p>
+                                            <p>".$row['n_nombre']."</p>
+                                            <p></p>
+                                            <p></p>
+                                            <div class='Btn__Green'>
+                                                <a href='../PHP/NivelRead.php?id=".trim($row['n_id'])."'>Ver</a>
+                                            </div>
+                                            <div class='Btn__Blue'>
+                                                <a href='../PHP/NivelUpdate.php?id=".trim($row['n_id'])."'>Actualizar</a>
+                                            </div>
+                                            <div class='Btn__Red'>
+                                                <a href='../PHP/NivelDelete.php?id=".trim($row['n_id'])."'>Eliminar</a>
+                                            </div>
+											<p></p>
+										";
+									}
+									Database::disconnect();
+								?>
+                            
+
+								
+				</div>
+			</form>
+
+		</main>
+
 	</body>
+	
 </html>
