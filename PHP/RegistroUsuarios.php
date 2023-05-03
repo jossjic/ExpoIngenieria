dec759a51b1d6a135d234ad485c8f3bcf661636e
Josafat <josafatgarciasarmientos@gmail.com>
2023-05-03 01:32:39 -0600

    Guardar Usuario Correo[id] en Edicon_COlaborador

--- PHP/RegistroUsuarios.php
+++ PHP/RegistroUsuarios.php
@@ -99,17 +99,17 @@
             
             $collaborator = $q->fetch(PDO::FETCH_ASSOC);
 
-            //Get the last edition able
-            $sql = "SELECT * FROM EDICION ORDER BY ed_id DESC LIMIT 1";
-            $q = $pdo->query($sql);
-            $last_edition_id = $q->fetch(PDO::FETCH_ASSOC);
+            // //Get the last edition able
+            // $sql = "SELECT * FROM EDICION ORDER BY ed_id DESC LIMIT 1";
+            // $q = $pdo->query($sql);
+            // $last_edition_id = $q->fetch(PDO::FETCH_ASSOC);
 
-            echo $last_edition_id[0]['ed_id'];
+            // echo $last_edition_id[0]['ed_id'];
 
-            //Insert into Edicion Colaborador with the last edition able on edition table
-            $sql = "INSERT INTO EDICION_COLABORADOR(co_correo,ed_id) VALUES(?,?)";
-            $q = $pdo->prepare($sql);
-            $q->execute(array($collaborator['co_correo'],$last_edition_id[0]['ed_id']));
+            // //Insert into Edicion Colaborador with the last edition able on edition table
+            // $sql = "INSERT INTO EDICION_COLABORADOR(co_correo,ed_id) VALUES(?,?)";
+            // $q = $pdo->prepare($sql);
+            // $q->execute(array($collaborator['co_correo'],$last_edition_id[0]['ed_id']));
 
             Database::disconnect();
             // Create session variables
