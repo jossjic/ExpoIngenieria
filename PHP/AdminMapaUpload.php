921f7be7bf23ec6e80285564d536e890ee89e41d
Josafat <josafatgarciasarmientos@gmail.com>
2023-05-04 14:51:38 -0600

    Admin Mapa

---
+++ PHP/AdminMapaUpload.php
@@ -0,0 +1,30 @@
+<?php
+	
+	require_once 'dataBase.php';
+	session_name("EngineerXpoWeb");
+    session_start();
+
+    if (!isset($_SESSION['logged_in'])) {
+		header("Location: ../index.php");
+		exit();
+	}
+
+	$URL = $_POST['url'];
+	$URL_Error = null;
+
+	$valid = true;
+
+	if (!empty($_POST)){
+		if ($valid) {
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "INSERT INTO MAPA(m_url) VALUES(?) ";
+			$q = $pdo->prepare($sql);
+			$q->execute(array($URL));
+			Database::disconnect();
+			header("Location: ../PHP/MapaProyectos.php");
+			exit();
+		}
+	}
+
+?>
