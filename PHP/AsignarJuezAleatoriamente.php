0b45fdaa4191e5eba36aa2464e0d09efa9a07ed4
Josafat <josafatgarciasarmientos@gmail.com>
2023-05-04 07:40:33 -0600

    Asignar Jurado

--- PHP/AsignarJuezAleatoriamente.php
+++ PHP/AsignarJuezAleatoriamente.php
@@ -12,37 +12,25 @@
 
     $conexion = Database::connect();
     // Obtener los proyectos y los profesores, y Jurado
-    $proyectos = $conexion->query("SELECT * FROM PROYECTO")->fetchAll(PDO::FETCH_ASSOC);
-    $jueces = $conexion->query("SELECT * FROM COLABORADOR WHERE co_es_jurado = true")->fetchAll(PDO::FETCH_ASSOC);
-    $totalJuecesProyecto = $proyectos/$jueces;
+    $sql = "SELECT * FROM PROYECTO NATURAL JOIN EDICION WHERE ed_id = ?";
+    $proyectos = $conexion->prepare($sql); 
+    $proyectos->execute(array($edicion['ed_id']));
+    $proyectoscount = $proyectoscount->rowCount();
 
-    // Crear un array vacío para los jueces asignados
-    $jueces_asignados = array();
+    $proyectos = $proyectos->fetchAll(PDO::FETCH_ASSOC);
 
-    // Recorrer cada proyecto
-    foreach ($proyectos as $proyecto) {
-        // Obtener los profesores que no están asociados con este proyecto
-        $jueces_disponibles = array_filter($jueces, function ($jueces) use ($proyecto) {
-            return $jueces['co_correo'] !== $jueces['profesor_1'] &&
-                $jueces['co_correo'] !== $jueces['profesor_2'] &&
-                $jueces['co_correo'] !== $proyecto['profesor_3'] &&
-                $jueces['co_correo'] !== $proyecto['profesor_4'];
-        });
+    $sql = "SELECT * FROM COLABORADOR NARTUAL JOIN EDICION COLABORADOR WHERE co_es_jurado = true AND ed_id = ?";
+    $jueces = $conexion->prepare($sql);
+    $jueces->execute(array($edicion['ed_id']));
+    $juecescount = $jueces->rowCount();
 
-        // Mezclar los profesores de forma aleatoria
-        shuffle($profesores_disponibles);
+    $jueces = $jueces->fetchAll(PDO::FETCH_ASSOC);
 
-        // Asignar los primeros 4 profesores aleatorios como jueces
-        $jueces_asignados[$proyecto['id']] = array_slice($profesores_disponibles, 0, 4);
-    }
+    Database::disconnect();
 
-    // Mostrar los jueces asignados para cada proyecto
-    foreach ($jueces_asignados as $proyecto_id => $jueces) {
-        echo "Jueces asignados al proyecto $proyecto_id: ";
-        foreach ($jueces as $juez) {
-            echo $juez['nombre'] . ", ";
-        }
-        echo "<br>";
-    }
+    $totalJuecesProyecto = $proyectoscount/$juecescount;
+
+    echo $totalJuecesProyecto;
+
 ?>
 
