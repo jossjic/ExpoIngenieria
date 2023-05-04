b097443444d31ed7e6cc9913c2cc61ae80a8852f
AugustoGM <103065341+AugustoGM@users.noreply.github.com>
2023-05-03 20:19:36 -0600

    Add files via upload

---
+++ PHP/generarReconocimientoG.php
@@ -0,0 +1,107 @@
+<?php
+require 'dataBase.php';
+require("../fpdf/fpdf.php");
+if ($_GET) {
+  if (isset($_GET['proyecto'])) {
+    $fpdf = new FPDF('P', 'mm', 'Letter');
+    $fpdf->AddPage();
+    $pdo = Database::connect();
+    //$sql = "SELECT * FROM alumno";
+    $sql = "SELECT ALUMNO.a_nombre, ALUMNO.a_apellido, PROYECTO.p_nombre, EDICION.ed_nombre, CATEGORIA.ca_nombre FROM ALUMNO JOIN PROYECTO_ALUMNO ON ALUMNO.a_correo = PROYECTO_ALUMNO.a_correo JOIN PROYECTO ON PROYECTO_ALUMNO.p_id = PROYECTO.p_id JOIN EDICION ON PROYECTO.ed_id = EDICION.ed_id JOIN CATEGORIA ON PROYECTO.ca_id = CATEGORIA.ca_id WHERE PROYECTO.p_id = {$_GET['proyecto']};";
+    $q = $pdo->query($sql);
+    for ($x = 0; $x < $q->rowCount(); $x++) {
+      //var_dump($q);
+      $filas = $q->fetch();
+      //var_dump($filas);
+      //Database::disconnect();
+
+      //var_dump($filas);
+      $nombre = $filas["a_nombre"] . " " . $filas["a_apellido"];
+      $proyecto = $filas["p_nombre"];
+      $edicion = $filas["ed_nombre"];
+      $categoria = $filas["ca_nombre"];
+      $tipo = "Reconocimiento";
+      $motivo = "por haber ganado en la";
+      $evento = "Expo ingeniería";
+      $dia = date("d");
+
+      //$mes = strftime("%A");
+      setlocale(LC_TIME, "spanish");
+      $mes = strftime("%B");
+
+      $year = date("Y");
+      $fecha = "Puebla, Puebla, a " . $dia . " de " . $mes . " de " . $year;
+      $autoridad1 = "Jorge Fco. Rocha Orozco";
+      $titulo1 = "Titulo1";
+      $autoridad2 = "José Rafael Aguilar Mejía";
+      $titulo2 = "Titulo2";
+      $campus = "campus Puebla";
+
+      $fpdf->SetFont('Arial', '', 16);
+      $fpdf->Ln(12);
+      $fpdf->Image('../media/Logotipo95.png', 60.45, null, 95, 0);
+      $fpdf->Ln(12);
+      $fpdf->Cell(0, 0, 'Otorga a:', 0, 0, 'C');
+      $nombre = iconv('UTF-8', 'windows-1252', $nombre);
+      $fpdf->Ln(18);
+      $fpdf->SetFont('Arial', 'B', 30);
+      $fpdf->Cell(0, 0, $nombre, 0, 0, 'C');
+      $fpdf->SetFont('Arial', 'I', 16);
+      $fpdf->Ln(15);
+      $fpdf->Cell(0, 0, "el presente", 0, 0, 'C');
+      $fpdf->Ln(15);
+      $fpdf->SetFont('Arial', 'IB', 24);
+      $tipo = iconv('UTF-8', 'windows-1252', $tipo);
+      $fpdf->Cell(0, 0, $tipo, 0, 0, 'C');
+      $fpdf->SetFont('Arial', 'I', 16);
+      $fpdf->Ln(15);
+      $motivo = iconv('UTF-8', 'windows-1252', $motivo);
+      $fpdf->Cell(0, 0, $motivo, 0, 0, 'C');
+      $fpdf->Ln(15);
+      $fpdf->SetFont('Arial', 'IB', 24);
+      $evento = iconv('UTF-8', 'windows-1252', $evento);
+      $fpdf->Cell(0, 0, $evento, 0, 0, 'C');
+      $fpdf->Ln(15);
+      $edicion = iconv('UTF-8', 'windows-1252', $edicion);
+      $fpdf->Cell(0, 0, $edicion, 0, 0, 'C');
+      $fpdf->SetFont('Arial', 'I', 16);
+      $fpdf->Ln(12);
+      $fpdf->Cell(0, 0, 'con el proyecto', 0, 0, 'C');
+      $fpdf->Ln(12);
+      $fpdf->SetFont('Arial', 'IB', 22);
+      $proyecto = iconv('UTF-8', 'windows-1252', $proyecto);
+      $fpdf->Cell(0, 0, $proyecto, 0, 0, 'C');
+      $fpdf->SetFont('Arial', 'I', 16);
+      $fpdf->Ln(12);
+      $fpdf->Cell(0, 0, 'en la categoria', 0, 0, 'C');
+      $fpdf->Ln(12);
+      $fpdf->SetFont('Arial', 'IB', 22);
+      $proyecto = iconv('UTF-8', 'windows-1252', $categoria);
+      $fpdf->Cell(0, 0, $proyecto, 0, 0, 'C');
+      $fpdf->Ln(15);
+
+      $fpdf->SetFont('Arial', '', 10);
+      $fecha = iconv('UTF-8', 'windows-1252', $fecha);
+      $fpdf->Cell(0, 0, $fecha, 0, 0, 'C');
+      $fpdf->Ln(22.4);
+      $fpdf->SetFont('Arial', 'BI', 12);
+      $autoridad1 = iconv('UTF-8', 'windows-1252', $autoridad1);
+      $titulo1 = iconv('UTF-8', 'windows-1252', $titulo1);
+      $campus = iconv('UTF-8', 'windows-1252', $campus);
+      $autoridad2 = iconv('UTF-8', 'windows-1252', $autoridad2);
+      $titulo2 = iconv('UTF-8', 'windows-1252', $titulo2);
+      $autoridades = array($autoridad1, '', $autoridad2);
+      $titulos = array($titulo1, '', $titulo2);
+      $ancho = array(83, 30, 83);
+      for ($i = 0; $i < count($autoridades); $i++)
+        $fpdf->Cell($ancho[$i], 7, $autoridades[$i], 0, 0, 'C');
+      $fpdf->Ln(5);
+      for ($i = 0; $i < count($autoridades); $i++)
+        $fpdf->Cell($ancho[$i], 7, $titulos[$i], 0, 0, 'C');
+      $fpdf->Ln(5);
+      $fpdf->SetFont('Arial', 'I', 12);
+      $fpdf->Cell($ancho[0], 7, $campus, 0, 0, 'C');
+    }
+    $fpdf->Output();
+  }
+}
---
+++ PHP/proyectosGanadoresA.php
@@ -0,0 +1,130 @@
+<?php 
+    require 'dataBase.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PROYECTOS GANADORES</title>
+
+        <link rel="stylesheet" href="../CSS/HeaderFooterStructure.css">
+        <link rel="stylesheet" href="../CSS/galeria.css">
+        
+	</head>
+
+    <header>
+        <img class="Logo__EscNegCie" src="../media/logotec-ings.svg" alt="Logo__EscNegCie">
+        <ul>
+        <li>
+            <a href="#">Layout Proyectos</a>
+        </li>
+        </ul>
+        <nav>
+        <ul>
+            <li><a href="#">Cerrar Sesión</a></li>
+        </ul>
+        </nav>
+    </header>
+
+	<body>
+	    <div class="container">
+
+            <div class="span10 offset1">
+                <br>
+                    <br>
+                    <h2>PROYECTOS GANADORES</h2>
+                    <br>
+                    <br>
+                    <table >
+
+                        <?php
+                            $pdo = Database::connect();
+                            //$sql = "SELECT PROYECTO.p_id, PROYECTO.p_nombre, EDICION.ed_id, EDICION.ed_nombre, ALUMNO.a_matricula, ALUMNO.a_nombre, ALUMNO.a_apellido, ALUMNO.a_correo FROM PROYECTO JOIN CATEGORIA ON PROYECTO.ca_id = CATEGORIA.ca_id JOIN EDICION ON PROYECTO.ed_id = EDICION.ed_id JOIN NIVEL ON PROYECTO.n_id = NIVEL.n_id JOIN PROYECTO_ALUMNO ON PROYECTO.p_id = PROYECTO_ALUMNO.p_id JOIN ALUMNO ON PROYECTO_ALUMNO.a_correo = ALUMNO.a_correo ORDER BY EDICION.ed_id,PROYECTO.p_id,ALUMNO.a_apellido,ALUMNO.a_nombre ASC;";
+                            $sql = "SELECT ALL (EVALUACION.ev_criterio_1 + EVALUACION.ev_criterio_2 + EVALUACION.ev_criterio_3 + EVALUACION.ev_criterio_4 + EVALUACION.ev_criterio_5) AS maximo, evaluacion.p_id, proyecto.p_id, proyecto.p_nombre, categoria.ca_nombre, edicion.ed_nombre, PROYECTO.ed_id, PROYECTO.ca_id, PROYECTO.p_descripcion FROM EVALUACION JOIN PROYECTO ON PROYECTO.p_id = EVALUACION.p_id JOIN CATEGORIA ON PROYECTO.ca_id = CATEGORIA.ca_id JOIN EDICION ON PROYECTO.ed_id = EDICION.ed_id ORDER BY PROYECTO.ed_id ASC, CATEGORIA.ca_nombre ASC, maximo DESC;";
+                            $q = $pdo->query($sql);
+                            $filas = $q->fetchAll();
+                            //var_dump($filas);
+                            Database::disconnect();
+                        ?>
+                        
+                        <tbody>
+                            <?php
+                            //$edicion = $filas[0]["ed_id"];
+                            //$proyecto = $filas[0]["p_id"];
+                            $edicion = "";
+                            $categoria = "";
+                            $proyecto = "";
+                            $total = "";
+                            //echo "<h2>" . $filas[0]["ed_nombre"] . "</h2>";
+                            //echo "<h3>" . $filas[0]["p_nombre"] . "</h3>";
+                            ?>
+                            <thead>
+                            
+                        </thead>
+                            <?php
+                            foreach ($filas as $datos) {
+                                $cadena = "<td>{$datos['p_id']}</td>
+                            <td>{$datos['p_nombre']}</td>
+                            <td>{$datos['p_descripcion']}</td>
+                            <td>{$datos['maximo']}</td>
+                            <br>" ;
+                            $link = '<a href="generarReconocimientoG.php?proyecto=' . $datos['p_id'] . '">Crear reconocimiento</a>';
+                                if ($datos["ed_id"] == $edicion) {
+                                    if($datos["ca_id"] == $categoria){
+
+                                        if ($total == $datos["maximo"]){
+                                            $total = $datos["maximo"];
+                                            echo $cadena;
+                                            echo $link;
+                                        }
+                                        else {
+                                            //proyectos no son max misma categoria y periodo
+                                            echo $cadena;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        $categoria = $datos["ca_id"];
+                                        echo "<h3>" . $datos["ca_nombre"] . "</h3>";
+                                        $total = $datos["maximo"];
+                                        echo $cadena;
+                                        echo $link;
+                                    }
+                                }
+                                else
+                                {
+                                    $edicion = $datos["ed_id"];
+                                    echo "<h2>" . $datos["ed_nombre"] . "</h2>";
+                                    if($datos["ca_id"] == $categoria){
+
+                                    }
+                                    else
+                                    {
+                                        $categoria = $datos["ca_id"];
+                                        echo "<h3>" . $datos["ca_nombre"] . "</h3>";
+                                        $total = $datos["maximo"];
+                                        echo $cadena;
+                                        echo $link;
+                                    }
+                                }  
+                            ?>
+
+                        </tbody>
+
+                    </table>
+                    
+                </div>
+                <?php
+                $total = $datos["maximo"];
+                                        }
+                ?>
+
+	    </div> <!-- /container -->
+        <br>
+        <br>
+        <a href="#">Volver</a>
+	</body>
+</html>
