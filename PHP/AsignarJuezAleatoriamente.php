d9a9ec53c58a0f9123dd27452eba0c0e09b0357c
Josafat <josafatgarciasarmientos@gmail.com>
2023-05-04 07:42:15 -0600

    Asignar Jurado

--- PHP/AsignarJuezAleatoriamente.php
+++ PHP/AsignarJuezAleatoriamente.php
@@ -8,29 +8,7 @@
 	$edicion = $q->fetch(PDO::FETCH_ASSOC);
 	Database::disconnect();
 
-
-
-    $conexion = Database::connect();
-    // Obtener los proyectos y los profesores, y Jurado
-    $sql = "SELECT * FROM PROYECTO NATURAL JOIN EDICION WHERE ed_id = ?";
-    $proyectos = $conexion->prepare($sql); 
-    $proyectos->execute(array($edicion['ed_id']));
-    $proyectoscount = $proyectoscount->rowCount();
-
-    $proyectos = $proyectos->fetchAll(PDO::FETCH_ASSOC);
-
-    $sql = "SELECT * FROM COLABORADOR NARTUAL JOIN EDICION COLABORADOR WHERE co_es_jurado = true AND ed_id = ?";
-    $jueces = $conexion->prepare($sql);
-    $jueces->execute(array($edicion['ed_id']));
-    $juecescount = $jueces->rowCount();
-
-    $jueces = $jueces->fetchAll(PDO::FETCH_ASSOC);
-
-    Database::disconnect();
-
-    $totalJuecesProyecto = $proyectoscount/$juecescount;
-
-    echo $totalJuecesProyecto;
+    echo $edicion['ed_id']
 
 ?>
 
